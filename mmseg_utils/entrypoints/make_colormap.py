928c49b746add933048d1b0437c70f3b7c649de8
David Russell <djrussell@ucdavis.edu>
2024-08-14 15:42:38 +0000

    Add make colormap entrypoint

---
+++ mmseg_utils/entrypoints/make_colormap.py
@@ -0,0 +1,16 @@
+import argparse
+
+from mmseg_utils.visualization.visualize_classes import show_colormaps
+
+def parse_args():
+    parser = argparse.ArgumentParser()
+    parser.add_argument("--cmap-name")
+    parser.add_argument("--class-names", nargs="+")
+    parser.add_argument("--savepath")
+
+    args = parser.parse_args()
+    return args
+
+if __name__ == "__main__":
+    args = parse_args()
+    show_colormaps(**args.__dict__)
