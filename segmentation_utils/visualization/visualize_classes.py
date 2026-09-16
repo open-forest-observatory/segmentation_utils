5f2d08a8c06dfb79ac8eb670f78559ef8c073d99
David Russell <djrussell@ucdavis.edu>
2026-09-16 17:24:21 +0000

    Visualization changes

--- segmentation_utils/visualization/visualize_classes.py
+++ segmentation_utils/visualization/visualize_classes.py
@@ -4,6 +4,7 @@ from pathlib import Path
 
 import matplotlib
 import matplotlib.pyplot as plt
+import matplotlib
 import numpy as np
 from tqdm import tqdm
 from PIL import Image
@@ -117,7 +118,7 @@ def show_colormaps(cmap_name, class_names, savepath=None):
             np.repeat(color, repeats=100, axis=0), repeats=100, axis=1
         )
         axs[i, j].imshow(vis_square)
-        axs[i, j].set_title(class_names[index], fontsize=8)
+        axs[i, j].set_title(class_names[index], fontsize=12, y=0.3)
         axs[i, j].axis("off")
     # Clear remaining subplots
     for index in range(num_classes, n_squares * n_squares):
@@ -129,6 +130,7 @@ def show_colormaps(cmap_name, class_names, savepath=None):
     if savepath is None:
         plt.show()
     else:
+        plt.tight_layout()
         plt.savefig(savepath)
         plt.close()
 
